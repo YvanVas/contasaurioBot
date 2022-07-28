cb93c3f4497220634578b3a008edcd59548e0517
Yvan <varelaivan30@gmail.com>
2022-07-28 11:42:46 -0400

    create_subdir

--- create_archives.py
+++ create_archives.py
@@ -25,11 +25,34 @@ def create_directory(dir_name: str, month=None, start_month=None, end_month=None
                 print('Subcarpetas creada: ', dir_month)
 
 
+def create_subdir(dir_name, start_month=None, end_month=None):
+
+    dir = 'Z:/r90/'+dir_name
+
+    if start_month and end_month is not None:
+
+        for i in range(start_month, end_month+1):
+
+            if i > 9:
+                dir_month = dir + '/' + str(i)
+            else:
+                dir_month = dir + '/' + '0'+str(i)
+
+            if os.path.exists(dir_month):
+                print('Ya existe: ', dir_month)
+            else:
+                os.mkdir(dir_month)
+
+                print('Subcarpetas creada: ', dir_month)
+
+
 def main() -> None:
 
     dir_name = input('Nombre de la carpeta: ')
 
-    create_directory(dir_name=dir_name, start_month=1, end_month=4)
+    #create_directory(dir_name=dir_name, start_month=5, end_month=12)
+
+    create_subdir(dir_name=dir_name, start_month=1, end_month=12)
 
 
 if __name__ == '__main__':
